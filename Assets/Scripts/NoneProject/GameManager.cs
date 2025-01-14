dec0b41c49b28fd3e650ed333b9f20e354955758
Raycast <hyeok0215@naver.com>
2025-01-15 02:27:50 +0900

    GameManager에서 최초 Const Data 로드하도록 수정

--- Assets/Scripts/NoneProject/GameManager.cs
+++ Assets/Scripts/NoneProject/GameManager.cs
@@ -1,17 +1,34 @@
-
+using Cysharp.Threading.Tasks;
 using NoneProject.Common;
-using NoneProject.Manager;
+using NoneProject.Data;
+using NoneProject.GameSystem;
 using Template.Manager;
-using UnityEngine;
-
 
 namespace NoneProject
 {
     public class GameManager : SingletonBase<GameManager>
     {
+        public InGame InGame { get; private set; }
+        
+        public ConstData Const { get; private set; }
+
         protected override void Initialized()
         {
-            IsInitialized = true;
+            base.Initialized();
+            
+            LoadConstData();
+            
+            isInitialized = true;
+        }
+
+        public void SetInGame(InGame inGame)
+        {
+            InGame = inGame;
+        }
+        
+        private void LoadConstData()
+        {
+            AddressableManager.Instance.LoadAssetsLabel<ConstData>(AddressableLabel.Common, asset =>  Const = asset).Forget();
         }
     }
 }
