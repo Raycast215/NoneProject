f17874bd5aae28030246c8f22a40389293ab7454
Raycast <hyeok0215@naver.com>
2025-01-15 02:09:12 +0900

    Sound Manager 코드정리

--- Assets/Scripts/Template/Manager/SoundManager.cs
+++ Assets/Scripts/Template/Manager/SoundManager.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using NoneProject;
+using NoneProject.Common;
+using NoneProject.Data;
 using NoneProject.Pool;
 using Template.Pool;
 using Template.Utility;
@@ -9,46 +12,57 @@ using UnityEngine;
 
 namespace Template.Manager
 {
+    // Scripted by Raycast
+    // 2025.01.14
+    // Bgm과 Sfx를 관리하는 클래스입니다.
+    
     public class SoundManager : SingletonBase<SoundManager>
     {
-        private const string Bgm = "Bgm";
-        private const string SoundPath = "Sound/SoundObject";
-        
         private event Action<AudioClip> OnBgmPlayed = delegate {  };
+
+        private ConstData ConstData => GameManager.Instance.Const;
         
         private readonly Dictionary<string, AudioClip> _audioClipDic = new Dictionary<string, AudioClip>();
         private readonly Dictionary<string, int> _sfxCountDic = new Dictionary<string, int>();
 
-        [SerializeField] private int sfxCapacity = 10;
-        [SerializeField] private int sfxLimitCount = 5;
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
         
         private AudioSource _bgmSource;
-        private CancellationTokenSource _cts = new CancellationTokenSource();
         private Pooling<SoundPool> _soundPool;
-        private SoundPool _sfxObject;
 
         /// BGM을 실행합니다.
         public void PlayBgm(string clipName, float volume = 1.0f, bool isLoop = true, bool isFade = false)
         {
+            // 받아온 Clip이 비어있는 경우 예외.
             if (string.IsNullOrEmpty(clipName))
+            {
+                Debug.Log($"[BGM] The '{clipName}' is null or empty...");
                 return;
-
-            Action<AudioClip> onBgmPlayed = null;
-            onBgmPlayed += ac => PlayBgmSound(ac, volume, isFade);
-
+            }
+            
+            // Bgm이 재생하면 실행할 이벤트 선언.
+            Action<AudioClip> onBgmPlayed = ac => PlayBgmSound(ac, volume, isFade);
+            
+            // Bgm의 Loop 설정.
             _bgmSource.loop = isLoop;
 
             if (_audioClipDic.TryGetValue(clipName, out var clip))
             {
-                // 동일한 Clip이 재생중이면 리턴.
+                // 동일한 Clip이 재생중인 경우 예외.
                 if (_bgmSource.clip == clip)
+                {
+                    Debug.Log($"[BGM] The '{clipName}' is already playing...");
                     return;
-
+                }
+                    
+                // Bgm 재생 이벤트 실행.
                 onBgmPlayed.Invoke(clip);
             }
             else
             {
+                // Bgm의 Clip이 없는 경우 Bgm 재생하면 Clip을 추가하는 이벤트 구독.
                 onBgmPlayed += ac => AddAudioClip(clipName, ac);
+                // Bgm의 Clip을 로드하고, 로드에 성공하면 Bgm 재생 이벤트를 실행함.
                 AddressableManager.Instance.LoadAsset<AudioClip>(clipName, onBgmPlayed.Invoke).Forget();
             }
         }
@@ -63,7 +77,7 @@ namespace Template.Manager
                 _sfxCountDic.Add(clipName, 1);
 
             // 동일한 Clip이 일정 수 이상 재생중인 경우 리턴.
-            if (_sfxCountDic[clipName] > sfxLimitCount)
+            if (_sfxCountDic[clipName] > ConstData.SfxDefaultLimitCount)
                 return;
             
             _sfxCountDic[clipName]++;
@@ -85,8 +99,8 @@ namespace Template.Manager
             }
         }
 
-        /// Sfx를 반환합니다.
-        public void Release(SoundPool sound)
+        /// Sfx를 Pool에 반환합니다.
+        public void ReleaseSfx(SoundPool sound)
         {
             _soundPool.Return(sound);
         }
@@ -150,8 +164,10 @@ namespace Template.Manager
                 if (_cts is null || _cts.IsCancellationRequested)
                     return;
                     
+                // 볼륨을 목표 볼륨에 따라 조절.
                 source.volume += Util.GetToggleOne(toVolume > 0.0f) * Time.deltaTime;
 
+                // 목표 볼륨에 근사하지 않은 경우 건너뜀.
                 if (Mathf.Approximately(source.volume, toVolume) is false) 
                     continue;
                     
@@ -162,9 +178,9 @@ namespace Template.Manager
             onFinished?.Invoke();
         }
         
-        private void CrateBgmObject()
+        private void InitializedBgm()
         {
-            _bgmSource ??= Util.CreateObject<AudioSource>(Bgm, transform, Vector3.zero);
+            _bgmSource ??= Util.CreateObject<AudioSource>($"{SoundTag.Bgm}", transform, Vector3.zero);
             _bgmSource.volume = 0.0f;
         }
         
@@ -176,9 +192,8 @@ namespace Template.Manager
 
         private void InitializedSfx()
         {
-            _sfxObject = Resources.Load<SoundPool>(SoundPath);
-            _soundPool = new Pooling<SoundPool>(_sfxObject, sfxCapacity, transform);
-            
+            var sfxObject = Resources.Load<SoundPool>(ConstData.SoundObjectPath);
+            _soundPool = new Pooling<SoundPool>(sfxObject, ConstData.SfxCapacity, transform);
             _soundPool.Pool();
         }
 
@@ -187,7 +202,7 @@ namespace Template.Manager
         protected override void Initialized()
         {
             InitializedSfx();
-            CrateBgmObject();
+            InitializedBgm();
             Subscribed();
 
             isInitialized = true;
@@ -197,8 +212,7 @@ namespace Template.Manager
         {
             _cts?.Cancel();
             _cts?.Dispose();
-            _cts = null;
-            
+         
             base.OnDestroy();
         }
         
