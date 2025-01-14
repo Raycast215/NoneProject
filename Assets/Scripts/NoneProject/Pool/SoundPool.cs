fccc597f5c20a6cb39281c931b6c8de471c11320
Raycast <hyeok0215@naver.com>
2025-01-15 02:10:06 +0900

    Sound Pool 코드정리

--- Assets/Scripts/NoneProject/Pool/SoundPool.cs
+++ Assets/Scripts/NoneProject/Pool/SoundPool.cs
@@ -1,29 +1,34 @@
-
+using System;
 using Cysharp.Threading.Tasks;
-using NoneProject.Manager;
+using Template.Manager;
 using Template.Pool;
 using UnityEngine;
-
 
 namespace NoneProject.Pool
 {
     public class SoundPool : PoolBase
     {
+        public event Action OnReleased = delegate {  }; 
+        
         [SerializeField] private AudioSource source;
         
         /// 받아온 clip으로 Sound를 재생하는 함수입니다. 
-        public async UniTaskVoid PlaySound(AudioClip clip)
+        public async UniTaskVoid PlaySound(AudioClip clip, float volume)
         {
             gameObject.SetActive(true);
             
             // clip 설정.
             source.clip = clip;
+            // Volume 설정.
+            source.volume = volume;
             // Sound 재생.
             source.Play();
 
-            // 재생이 끝날 때까지 대기.
-            await UniTask.WaitUntil(() => source.isPlaying is false, cancellationToken: Cts.Token);
-
+            await UniTask.WaitForSeconds(source.clip.length, cancellationToken: Cts.Token);
+            
+            if (Cts is null || Cts.IsCancellationRequested)
+                return;
+            
             Release();
         }
 
@@ -34,8 +39,11 @@ namespace NoneProject.Pool
             // Clip 초기화.
             source.clip = null;
             
+            OnReleased?.Invoke();
+            OnReleased = null;
+            
             // Pool 해제.
-            PoolManager.Instance.SoundPool.Return(this);
+            SoundManager.Instance.ReleaseSfx(this);
         }
 
 #endregion
