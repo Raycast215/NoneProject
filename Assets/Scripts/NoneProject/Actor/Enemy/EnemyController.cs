0efd0fce9513944736cc43d5c7c04d38434bd9ed
Raycast <hyeok0215@naver.com>
2025-01-16 03:23:04 +0900

    Enemy의 이동 로직을 처리하는 클래스 추가

---
+++ Assets/Scripts/NoneProject/Actor/Enemy/EnemyController.cs
@@ -0,0 +1,66 @@
+using NoneProject.Actor.Behaviour;
+using NoneProject.Common;
+using Template.Utility;
+using UnityEngine;
+
+namespace NoneProject.Actor.Enemy
+{
+    // Scripted by Raycast
+    // 2025.01.16
+    // Enemy를 관리하는 클래스입니다.
+    public class EnemyController : ActorBase
+    {
+        private EnemyMoveBehaviour _moveBehaviour;
+        private ModelAnimationBehaviour _modelAnimationBehaviour;
+        
+        public string Name { get; private set; }
+
+        private void FixedUpdate()
+        {
+            if (gameObject.activeInHierarchy is false)
+                return;
+            
+            _moveBehaviour.Move();
+        }
+
+        // To Do: 나중에 데이터로 불러오기
+        public void SetName(string enemyName)
+        {
+            Name = enemyName;
+        }
+
+        private void Subscribed()
+        {
+            _moveBehaviour.OnAnimationStateChanged += state => _modelAnimationBehaviour.SetAnimationState(state);
+            _moveBehaviour.Subscribe();
+        }
+        
+        public void SetPosition(Vector2 position, bool isRandom = false)
+        {
+            var pos = isRandom 
+                ? Util.GetRandomDirVec(transform.position, 2.0f, 2.0f) 
+                : position;
+            
+            _moveBehaviour.SetPosition(pos);
+        }
+
+        public override void Initialized()
+        {
+            base.Initialized();
+            
+            _modelAnimationBehaviour = new ModelAnimationBehaviour(Model);
+            _moveBehaviour = new EnemyMoveBehaviour(Rigidbody2D);
+            _moveBehaviour.SetPattern(MovePattern.Random);
+            _moveBehaviour.SetMoveSpeed(1.0f);
+            
+            Subscribed();
+            
+            IsLoaded = true;
+        }
+
+        public override void Move(float moveSpeed = 1.0f, Vector2 moveVec = new Vector2())
+        {
+            _moveBehaviour.Move();
+        }
+    }
+}
