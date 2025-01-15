469da3f8c94eeb6b0f232fb5e180015aed0ab250
Raycast <hyeok0215@naver.com>
2025-01-16 03:23:11 +0900

    Enemy의 이동 로직을 처리하는 클래스 추가

---
+++ Assets/Scripts/NoneProject/Actor/Enemy/EnemyMoveBehaviour.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using NoneProject.Actor.Behaviour;
+using NoneProject.Actor.Move;
+using NoneProject.Common;
+using NoneProject.Interface;
+using UnityEngine;
+
+namespace NoneProject.Actor.Enemy
+{
+    // Scripted by Raycast
+    // 2025.01.16
+    // Enemy의 이동 로직을 처리하는 클래스입니다.
+    public class EnemyMoveBehaviour : MoveBehaviour
+    {
+        public event Action<ActorState> OnAnimationStateChanged;
+
+        private Dictionary<MovePattern, IMovable> _movePatternDic;
+
+        private MovePattern _movePattern;
+        private readonly AutoMove _autoMove;
+    
+        public EnemyMoveBehaviour(Rigidbody2D rigidbody2D)
+        {
+            Rigidbody = rigidbody2D;
+            _autoMove = new AutoMove(rigidbody2D);
+            
+            _movePatternDic = new Dictionary<MovePattern, IMovable>()
+            {
+                {MovePattern.Random, _autoMove}
+            };
+        }
+
+        public void SetPattern(MovePattern movePattern)
+        {
+            _movePattern = movePattern;
+        }
+
+        public void Subscribe()
+        {
+            _autoMove.OnMoveVecUpdated += _ => OnAnimationStateChanged?.Invoke(ActorState.Run);
+            _autoMove.OnDirectionUpdated += SetDirection;
+        }
+
+#region Override Methods
+        
+        public override void Move(float moveSpeed = 1.0f, Vector2 moveVec = new Vector2())
+        {
+            // 이동 실행.
+            _autoMove.Move(MoveSpeed);
+        }
+        
+#endregion
+    }
+}
