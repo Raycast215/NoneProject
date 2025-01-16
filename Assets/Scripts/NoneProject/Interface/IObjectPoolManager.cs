96446ea29d247a03ee4676381230e1296b2520b5
Raycast <hyeok0215@naver.com>
2025-01-17 07:21:18 +0900

    Pool 매니저 인터페이스 제네릭 타입 수정 및 함수 수정

--- Assets/Scripts/NoneProject/Interface/IObjectPoolManager.cs
+++ Assets/Scripts/NoneProject/Interface/IObjectPoolManager.cs
@@ -1,3 +1,5 @@
+using Cysharp.Threading.Tasks;
+using Template.Pool;
 using UnityEngine;
 
 namespace NoneProject.Interface
@@ -5,12 +7,9 @@ namespace NoneProject.Interface
     // Scripted by Raycast
     // 2025.01.17
     // 오브젝트 풀을 사용하는 매니저 클래스에서 공통으로 사용할 함수를 묶어놓은 인터페이스입니다.
-    public interface IObjectPoolManager<in T, in TU> where T : Component where TU : Component
+    public interface IObjectPoolManager<in T, TU> where T : PoolBase<TU> where TU : Component
     {
-        public void Get(string poolObjectID);
-        public void SetController(string poolObjectID, TU controller);
+        public UniTask<TU> Get(string poolObjectID);
         public void Release(T poolObject);
-        public void LoadPool();
-        public void CreateObjectHolder();
     }
 }
