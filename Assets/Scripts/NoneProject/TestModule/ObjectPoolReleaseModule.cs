e4ec4f4f1c2b212396bac52be7f91ab98a996da8
Raycast <hyeok0215@naver.com>
2025-01-17 09:28:03 +0900

    Release 모듈 예외 추가

--- Assets/Scripts/NoneProject/TestModule/ObjectPoolReleaseModule.cs
+++ Assets/Scripts/NoneProject/TestModule/ObjectPoolReleaseModule.cs
@@ -16,7 +16,16 @@ namespace NoneProject.TestModule
         private void Release()
         {
             if (Application.isPlaying is false)
+            {
+                Debug.Log("[Test Module] Application isPlaying is false...");
                 return;
+            }
+
+            if (OnReleased is null)
+            {
+                Debug.Log("[Test Module] Release Module Event is null...");
+                return;
+            }
             
             OnReleased?.Invoke();
         }
