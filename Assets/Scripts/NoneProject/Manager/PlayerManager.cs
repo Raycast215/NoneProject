49f1f953a6176fd8a45137314f97f6fd6733ce2c
Raycast <hyeok0215@naver.com>
2025-01-25 01:45:39 +0900

    PlayerManager 리팩토링

--- Assets/Scripts/NoneProject/Manager/PlayerManager.cs
+++ Assets/Scripts/NoneProject/Manager/PlayerManager.cs
@@ -8,12 +8,12 @@ namespace NoneProject.Manager
 {
     // Scripted by Raycast
     // 2025.01.15
-    // Player 및 Enemy을 생성하고 관리하는 클래스입니다.
-    public class ActorManager : SingletonBase<ActorManager>
+    // Player 생성하고 관리하는 클래스입니다.
+    public class PlayerManager : SingletonBase<PlayerManager>
     {
         public PlayerController Player { get; private set; }
 
-        public async void LoadPlayer(Transform playerHolder, Action<PlayerController> onComplete = null)
+        private async void Load(Action<PlayerController> onComplete = null)
         {
             // Player의 에셋을 로드.
             await AddressableManager.Instance.LoadAssetsLabel<GameObject>(AddressableLabel.Player, OnLoadComplete);
@@ -22,9 +22,11 @@ namespace NoneProject.Manager
             void OnLoadComplete(GameObject prefab)
             {
                 // 오브젝트 생성.
-                Player = Instantiate(prefab, playerHolder).GetComponent<PlayerController>();
+                Player = Instantiate(prefab, transform).GetComponent<PlayerController>();
                 // Player 생성 후 실행할 이벤트 실행.
                 onComplete?.Invoke(Player);
+                
+                isInitialized = true;
             }
         }
 
@@ -33,8 +35,8 @@ namespace NoneProject.Manager
         protected override void Initialized()
         {
             base.Initialized();
-            
-            isInitialized = true;
+
+            Load();
         }
         
 #endregion
