0536538803bd20f0fc8dd198b9ca269843e97a05
hyeok0215 <hyoke0215@naver.com>
2025-02-03 15:35:59 +0900

    UI로 사용되는 오브젝트에서 사용할 클래스 추가

---
+++ Assets/Scripts/NoneProject/UI/UiObject.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace NoneProject.Ui
+{
+    // Scripted by Raycast
+    // 2025.02.03
+    // UI로 사용되는 오브젝트에서 사용할 클래스.
+    public abstract class UiObject : MonoBehaviour
+    {
+        public abstract void Subscribe();
+    }
+}
