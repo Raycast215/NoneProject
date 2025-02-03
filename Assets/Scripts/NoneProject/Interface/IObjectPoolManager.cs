a51ac392c094613ef9e2820026bd7eb52d98253a
hyeok0215 <hyoke0215@naver.com>
2025-02-04 07:24:43 +0900

    미사용 함수 제거

--- Assets/Scripts/NoneProject/Interface/IObjectPoolManager.cs
+++ Assets/Scripts/NoneProject/Interface/IObjectPoolManager.cs
@@ -1,4 +1,3 @@
-using Cysharp.Threading.Tasks;
 using Template.Pool;
 using UnityEngine;
 
@@ -9,7 +8,6 @@ namespace NoneProject.Interface
     // 오브젝트 풀을 사용하는 매니저 클래스에서 공통으로 사용할 함수를 묶어놓은 인터페이스입니다.
     public interface IObjectPoolManager<in T, TU> where T : PoolBase<TU> where TU : Component
     {
-        public UniTask<TU> Get(string poolObjectID);
         public void Release(T poolObject);
     }
 }
